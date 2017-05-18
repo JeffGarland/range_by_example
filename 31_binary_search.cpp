fdf4f88b8a395549a112fe4473eec99763ed8ea4
Jeff Trull <jeff.trull@ciere.com>
2017-05-18 13:03:01 -0700

    Add binary_search example

---
+++ 31_binary_search.cpp
@@ -0,0 +1,39 @@
+/* output
+*/
+
+#include <range/v3/algorithm/reverse.hpp>
+#include <range/v3/view/iota.hpp>
+#include <range/v3/algorithm/binary_search.hpp>
+namespace rng = ranges::v3;
+
+#include <vector>
+#include <iostream>
+using namespace std;
+
+auto print = [] (int i) { cout << i << " "; };
+
+int main() {
+
+  vector<int> v { 1, 2, 3, 3, 4, 5 };     // inputs must be sorted but may be repeated
+
+  assert(rng::binary_search(v, 3));       // returns whether item is found
+  assert(!rng::binary_search(v, 10));
+
+  // you can supply your own comparison predicate
+  rng::reverse(v);
+  assert(rng::binary_search(v,   3, [](int a, int b) { return b < a; })); // reversed comparison
+  assert(!rng::binary_search(v, 10, [](int a, int b) { return b < a; }));
+
+  // and a custom "projection" function to be applied prior to the comparison
+  // this one inverts the value so the range is sorted in increasing (less negative) order
+  assert(rng::binary_search(v,   -3, std::less<>{}, [](int x) { return -x; }));
+  assert(!rng::binary_search(v, -10, std::less<>{}, [](int x) { return -x; }));
+
+  // "error: static assertion failed: Trying to binary search an infinite range":
+  // rng::binary_search(rng::view::iota(1), 12);
+  
+  // finite ranges are OK
+  assert(rng::binary_search(rng::view::iota(1, 6), 5));
+  assert(!rng::binary_search(rng::view::iota(1, 6), 7));
+
+}
