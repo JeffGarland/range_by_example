468b8e29c7bcfbf171672907d75213a52cee98eb
JeffGarland <jeff@crystalclearsoftware.com>
2017-05-18 10:26:41 -0600

    add is_sorted

---
+++ 10_is_sorted.cpp
@@ -0,0 +1,29 @@
+/* output
+vector:   true
+array:    false
+*/
+
+#include <range/v3/algorithm/is_sorted.hpp>   //specific includes
+namespace rng = ranges::v3;
+
+#include <vector>
+#include <array>
+#include <iostream>
+using namespace std;
+
+auto is_six = [](int i) -> bool { return i == 6; };
+
+int main() {
+
+  cout << "vector:   ";
+
+  vector<int> v { 1, 2, 3, 4, 5, 6 };
+  if (rng::is_sorted( v )) { cout << "true" << endl; };
+  
+  cout << "array:    ";
+
+  array<int, 6> a { 6, 2, 3, 4, 5, 6 };
+  if (!rng::is_sorted( a )) { cout << "false" << endl; };
+  
+}
+
