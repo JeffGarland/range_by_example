c2c3c91ad2dfb7c40daa80b39bbccd8f8658497d
JeffGarland <jeff@crystalclearsoftware.com>
2017-05-18 10:36:37 -0600

    add max_min

--- 04_count.cpp
+++ 04_count.cpp
@@ -1,5 +1,3 @@
-// /home/jeff/devtools/gcc7/install/usr/local/bin/g++ -I /home/jeff/devtools/range_git/range-v3/include -std=c++17 04_count.cpp
-
 /* output
 vector:   2
 array:    2
@@ -10,11 +8,6 @@ namespace rng = ranges::v3;
 
 #include <vector>
 #include <deque>
-#include <list>
-#include <forward_list>
-#include <stack>
-#include <queue>
-#include <array>
 #include <iostream>
 using namespace std;
 
@@ -31,24 +24,7 @@ int main() {
   array<int, 6> a { 6, 2, 3, 4, 5, 6 };
   c = rng::count( a, 6 );
   
-
   cout << c << endl;
   
-  // list<int> ll { 1, 2, 3, 4, 5, 6 };
-  // rng::for_each( ll, print );
-
-  // cout << endl << "fwd_list: ";
-
-  // forward_list<int> fl { 1, 2, 3, 4, 5, 6 };
-  // rng::for_each( fl, print ); 
-
-  // cout << endl << "deque:    ";
-  
-  // deque<int> d { 1, 2, 3, 4, 5, 6 };
-  // rng::for_each( d, print ); 
-
-  // cout << endl;
-
-
 }
 
