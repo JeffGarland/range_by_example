5f6257a0a9823e25c1be857aa69cae0869b7e317
André Bergner <andre.bergner@native-instruments.de>
2017-05-18 21:31:20 +0200

    Adding view example for python style zip.

---
+++ 50_zip.cpp
@@ -0,0 +1,23 @@
+/* output
+a : 1
+b : 2
+c : 3
+d : 4
+e : 5
+*/
+
+#include <range/v3/all.hpp>         // get everything
+namespace view = ranges::v3::view;  // easy access
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+
+  string letters = { "abcde" };
+  auto   numbers = { 1, 2, 3, 4, 5 };
+
+  for (auto [l,n] : view::zip(letters, numbers))
+    cout << l << " : " << n << endl;
+}
