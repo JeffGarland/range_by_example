8d3d4c50fc40253041665c42f5bac073030cd76c
JeffGarland <jeff@crystalclearsoftware.com>
2017-05-19 09:00:27 -0600

    restore set diff examples -- clean up compiler warnings due to unused vars

--- 40_set_algos.cpp
+++ 40_set_algos.cpp
@@ -9,7 +9,6 @@ symmetric_difference: 5 6 7 8
 */
 
 #include <range/v3/algorithm/set_algorithm.hpp>
-
 namespace rng = ranges::v3;  //easy access
 
 #include <iostream>
@@ -47,29 +46,25 @@ int main() {
     cout << "intersection: ";
     output(u);
   }
-  // compile error on gcc7 pre-release
- // could not convert ‘((const ranges::v3::copy_fn*)(& ranges::v3::{anonymous}::copy))->ranges::v3::copy_fn::operator()<__gnu_cxx::__normal_iterator<const int*, std::vector<int> >,
-  // {
-  //   vector<int> u;
-  //   auto it = rng::set_difference(
-  //       s1.cbegin(), s1.cend(), s2.cbegin(), s2.cend(), rng::back_inserter(u));
-  //   cout << "difference(s1,s2): ";
-  //   output(u);
-  // }
-
-  // {
-  //   vector<int> u;
-  //   auto it = rng::set_difference(
-  // 				  s2.cbegin(), s2.cend(), s1.cbegin(), s1.cend(), rng::back_inserter(u));
-  //       s2, s1, rng::back_inserter(u));
-  //   cout << "difference(s2,s1): ";
-  //   output(u);
-  // }
+  {
+    vector<int> u;
+    rng::set_difference(s1, s2, rng::back_inserter(u));
+    cout << "difference(s1,s2): ";
+    output(u);
+  }
 
   {
     vector<int> u;
-    rng::set_symmetric_difference(
-        s2.cbegin(), s2.cend(), s1.cbegin(), s1.cend(), rng::back_inserter(u));
+    //an example of the interator based call
+    rng::set_difference(
+			s2.cbegin(), s2.cend(), s1.cbegin(), s1.cend(), rng::back_inserter(u));
+    cout << "difference(s2,s1): ";
+    output(u);
+  }
+
+  {
+    vector<int> u;
+    rng::set_symmetric_difference(s2, s1, rng::back_inserter(u));
     cout << "symmetric_difference: ";
     output(u);
   }
